1f318ee958436801c214cc753b72dadd91081f2d
useung73 <useung73@gmail.com>
2022-12-05 10:44:46 +0900

    태그 페이지 만들기

--- blog/tests.py
+++ blog/tests.py
@@ -43,7 +43,7 @@ class TestView(TestCase) :
         self.post_003.tags.add(self.tag_python)
 
          # 카테고리 페이지 테스트 코드
-        def test_category_page(self) :
+        def test_category_page(self):
             response = self.client.get(self.category_programming.get_absolute_url())
             self.assertEqual(response.status_code, 200)
 
@@ -172,3 +172,20 @@ class TestView(TestCase) :
             self.assertIn(self.tag_hello.name, post_area.text)
             self.assertNotIn(self.tag_python_name, post_area.text)
             self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
+        def test_tag_page(self):
+            response = self.client.get(self.tag_hello.get_absolute_url())
+            self.assertEqual(response.status_code, 200)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            self.navbar_test(soup)
+            self.category_card_test(soup)
+
+            self.assertIn(self.tag_hello.name, soup.h1.text)
+
+            main_area = soup.find('div',id='main-area')
+            self.assertIn(self.tag_hello.name, main_area.text)
+
+            self.assertIn(self.post001.title, main_area.text)
+            self.assertNotIn(self.post002.title, main_area.text)
+            self.assertNotIn(self.post003.title, main_area.text)
